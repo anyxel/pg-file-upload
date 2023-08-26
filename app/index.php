282b2e95257a58130f926b8951ce7c5f45911bed
Md Obydullah <hi@obydul.me>
2023-08-26 22:09:46 +0600

    updated index file

--- app/index.php
+++ app/index.php
@@ -6,8 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="robots" content="noindex" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-  <title>Anyxel Playground File Upload</title>
-  <link rel="icon" href="https://cdn.shouts.dev/media/357/linux.png">
+  <title>File Upload Playground - Anyxel</title>
+  <link rel="icon" href="https://cdn.shouts.dev/media/365/anyxel-black-github.png">
   <style>
     html,
     body {
@@ -24,8 +24,8 @@
 <body>
   <div class="container h-100 d-flex align-items-center justify-content-center">
     <div class="text-center">
-      <img src="https://cdn.shouts.dev/media/357/linux.png" width="120px">
-      <h1 class="display-6 mt-4">Anyxel Playground File Upload</h1>
+      <img src="https://cdn.shouts.dev/media/365/anyxel-black-github.png" width="120px">
+      <h1 class="display-6 mt-4">File Upload Playground</h1>
       <p class="text-muted mt-4"><small>© <a href="https://anyxel.com" target="_blank">Anyxel</a> <span id="fullYear"></span></small></p>
     </div>
   </div>
